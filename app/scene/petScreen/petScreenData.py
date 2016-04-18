74f007fe59c79b9ed0b8633302e2f41ceddaa9be
Nalia1991 <marie.44.56@hotmail.com>
2016-04-17 23:53:19 -0400

    Tree is now doable.

--- app/scene/petScreen/petScreenData.py
+++ app/scene/petScreen/petScreenData.py
@@ -9,19 +9,22 @@ class PetScreenData:
         self.gameData = gameData
         self.allSprites = pygame.sprite.Group()
 
-        self.messageLog = MessageBox(2*SCREEN_WIDTH/3,SCREEN_HEIGHT/9,SCREEN_WIDTH/2,8*SCREEN_HEIGHT/9)
+        self.messageLog = MessageBox(2*SCREEN_WIDTH/3,SCREEN_HEIGHT/8,SCREEN_WIDTH/2,8*SCREEN_HEIGHT/9)
         self.messageLog.textList.append('Your pet is hungry. Get him something.')
         self.allSprites.add(self.messageLog)  # Add sprite
 
         #All item
         self.itemInfoList = gameData.itemInfoList
 
-        # #For testing purpose
-        # for item in self.gameData.itemInfoList.item:
-        #     self.gameData.itemInfoList.item[item].unlock = True
-        #     self.itemInfoList.item[item].inventory = 10
-        # #self.gameData.itemInfoList.item['apple'].unlock = True
-        # #self.gameData.itemInfoList.item['carrot'].unlock = True
+        #For testing purpose
+        for item in self.gameData.itemInfoList.item:
+             self.gameData.itemInfoList.item[item].unlock = True
+             self.itemInfoList.item[item].inventory = 16
+        # self.gameData.itemInfoList.item['apple'].unlock = False
+        # self.gameData.itemInfoList.item['carrot'].unlock = False
+        # self.gameData.itemInfoList.item['gun'].unlock = False
+        # self.gameData.itemInfoList.item['pokerChip'].unlock = False
+        # self.gameData.itemInfoList.item['totem'].unlock = False
 
         #All pet
         self.petTypeList = gameData.petList
